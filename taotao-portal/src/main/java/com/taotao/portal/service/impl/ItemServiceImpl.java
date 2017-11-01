243c979a60986b5d66adc570931055701f319616
chenjunfeng <chenjunfengf@163.com>
2017-11-01 20:55:32 +0800

    修改忽略文件配置.gitignore

--- taotao-portal/src/main/java/com/taotao/portal/service/impl/ItemServiceImpl.java
+++ taotao-portal/src/main/java/com/taotao/portal/service/impl/ItemServiceImpl.java
@@ -1,131 +1,104 @@
-<div id="attributes" class="attributes" style="margin:0px;padding:0px;font-family:tahoma, arial, 微软雅黑, sans-serif;background-color:#FFFFFF;">
-
-	<div class="attributes-list" id="J_AttrList" style="margin:0px 0px 10px;padding:0px;">
-
-		<div>
-
-			<span><br />
-
-</span>
-
-		</div>
-
-	</div>
-
-</div>
-
-<div id="mall-banner" style="margin:0px;padding:0px;color:#404040;font-family:tahoma, arial, 微软雅黑, sans-serif;background-color:#FFFFFF;">
-
-	<div style="margin:0px;padding:0px;">
-
-	</div>
-
-	<div id="J_DescTMS1" style="margin:0px;padding:0px;">
-
-	</div>
-
-</div>
-
-<div id="J_TmpActBanner" style="margin:0px;padding:0px;color:#404040;font-family:tahoma, arial, 微软雅黑, sans-serif;background-color:#FFFFFF;">
-
-</div>
-
-<div id="J_DcTopRightWrap" style="margin:0px;padding:0px;color:#404040;font-family:tahoma, arial, 微软雅黑, sans-serif;background-color:#FFFFFF;">
-
-	<div id="J_DcTopRight" class="J_DcAsyn tb-shop" style="margin:0px;padding:0px;">
-
-		<div class="J_TModule" id="shop13677347667" style="margin:0px;padding:0px;">
-
-			<div class="skin-box tb-module tshop-pbsm tshop-pbsm-shop-self-defined" style="margin:0px;padding:0px;font-family:微软雅黑, tahoma, arial;color:#666666;">
-
-				<s class="skin-box-tp"><b></b></s>
-
-				<div class="skin-box-bd clear-fix" style="margin:0px;padding:0px;border:0px solid #D5D5D5;background-color:transparent;color:#2953A6;">
-
-					<span>
-
-					<div class="psrel lcr-box" style="margin:0px;padding:0px;background-color:transparent;">
-
-						<a class="psabs utools_module hotarea hotarea_1 lcr-hotarea" href="https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2766164095&amp;activityId=dab23422f11e494287078b4fb039121f&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_2 lcr-hotarea" href="https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2766164095&amp;activityId=a2dacd2957704acfa1ded7cfb4789fd2&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_3 lcr-hotarea" href="https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2766164095&amp;activityId=89cdb2f665354f88b48de01f58a43d91&amp;scene=taobao_shop" target="_blank"></a>
-
-					</div>
-
-</span>
-
-				</div>
-
-<s class="skin-box-bt"><b></b></s>
-
-			</div>
-
-		</div>
-
-		<div class="J_TModule" id="shop13333405586" style="margin:0px;padding:0px;">
-
-			<div class="skin-box tb-module tshop-pbsm tshop-pbsm-shop-self-defined" style="margin:0px;padding:0px;font-family:微软雅黑, tahoma, arial;color:#666666;">
-
-				<s class="skin-box-tp"><b></b></s>
-
-				<div class="skin-box-bd clear-fix" style="margin:0px;padding:0px;border:0px solid #D5D5D5;background-color:transparent;color:#2953A6;">
-
-					<span>
-
-					<div class="psrel lcr-box" style="margin:0px;padding:0px;background-color:transparent;">
-
-						<a class="psabs utools_module image image_1" target="_blank"><span><img src="https://gdp.alicdn.com/imgextra/i1/2766164095/TB277NtlVXXXXbsXpXXXXXXXXXX_!!2766164095.jpg" /></span></a><a class="psabs utools_module image image_2" target="_blank"><span><img src="https://gdp.alicdn.com/imgextra/i4/2766164095/TB2MTV5lVXXXXXtXXXXXXXXXXXX_!!2766164095.jpg" /></span></a><a class="psabs utools_module image image_3" target="_blank"><span><img src="https://gdp.alicdn.com/imgextra/i2/2766164095/TB24kFslVXXXXbUXpXXXXXXXXXX_!!2766164095.jpg" /></span></a><a class="psabs utools_module hotarea hotarea_1 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.8.HyjgTN&amp;id=526516902802&amp;rn=b3ee79a6aeeeed4d5a24c240ffdd8c38&amp;abbucket=13&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_2 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.9.HyjgTN&amp;id=526526113368&amp;&amp;skuId=3134701249810&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_3 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.8.HyjgTN&amp;id=526516902802&amp;rn=b3ee79a6aeeeed4d5a24c240ffdd8c38&amp;abbucket=13&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_4 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.10.HyjgTN&amp;id=526480283927&amp;rn=bb80bb5def2143c04dbd15720ac7ac9b&amp;abbucket=11&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_5 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.1.HyjgTN&amp;id=526526025119&amp;rn=36ffb8e437f44f9093a824f509bce22c&amp;abbucket=19&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_6 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.3.HyjgTN&amp;id=526525637198&amp;rn=36ffb8e437f44f9093a824f509bce22c&amp;abbucket=19&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_7 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.2.HyjgTN&amp;id=526517894879&amp;rn=36ffb8e437f44f9093a824f509bce22c&amp;abbucket=19&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_8 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.4.HyjgTN&amp;id=526517894879&amp;rn=36ffb8e437f44f9093a824f509bce22c&amp;abbucket=19&amp;skuId=3134704777162&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_9 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.11.HyjgTN&amp;id=526515291417&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_10 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.12.HyjgTN&amp;id=526484287074&amp;rn=36ffb8e437f44f9093a824f509bce22c&amp;abbucket=19&amp;scene=taobao_shop" target="_blank"></a><a class="psabs utools_module hotarea hotarea_11 lcr-hotarea" href="https://detail.tmall.com/item.htm?spm=a220o.1000855.w5003-13333405586.13.HyjgTN&amp;id=526561105531&amp;scene=taobao_shop" target="_blank"></a>
-
-					</div>
-
-</span>
-
-				</div>
-
-<s class="skin-box-bt"><b></b></s>
-
-			</div>
-
-		</div>
-
-		<div class="J_TModule" id="shop13333479526" style="margin:0px;padding:0px;">
-
-			<div class="skin-box tb-module tshop-pbsm tshop-pbsm-shop-self-defined" style="margin:0px;padding:0px;font-family:微软雅黑, tahoma, arial;color:#666666;">
-
-				<s class="skin-box-tp"><b></b></s>
-
-				<div class="skin-box-bd clear-fix" style="margin:0px;padding:0px;border:0px solid #D5D5D5;background-color:transparent;color:#2953A6;">
-
-					<span>
-
-					<p>
-
-						<img src="https://gdp.alicdn.com/imgextra/i2/2766164095/TB2mRJekXXXXXXnXFXXXXXXXXXX_!!2766164095.jpg" />
-
-					</p>
-
-</span>
-
-				</div>
-
-<s class="skin-box-bt"><b></b></s>
-
-			</div>
-
-		</div>
-
-	</div>
-
-</div>
-
-<div id="description" class="J_DetailSection tshop-psm tshop-psm-bdetaildes" style="margin:0px;padding:0px;color:#404040;font-family:tahoma, arial, 微软雅黑, sans-serif;background-color:#FFFFFF;">
-
-	<div class="content ke-post" style="margin:10px 0px 0px;padding:0px;font-size:14px;font-family:tahoma, arial, 宋体, sans-serif;">
-
-		<p style="text-align:center;">
-
-			<img align="absmiddle" src="https://img.alicdn.com/imgextra/i2/2766164095/TB2Qk79jVXXXXbaXXXXXXXXXXXX_!!2766164095.jpg" class="img-ks-lazyload" /><img align="absmiddle" src="https://img.alicdn.com/imgextra/i4/2766164095/TB28oEUjVXXXXXsXpXXXXXXXXXX_!!2766164095.jpg" class="img-ks-lazyload" /><img align="absmiddle" src="https://img.alicdn.com/imgextra/i2/2766164095/TB2Hik2jVXXXXbDXXXXXXXXXXXX_!!2766164095.jpg" class="img-ks-lazyload" /><img align="absmiddle" src="https://img.alicdn.com/imgextra/i2/2766164095/TB220QKjVXXXXaqXpXXXXXXXXXX_!!2766164095.jpg" class="img-ks-lazyload" />
-
-		</p>
-
-	</div>
-
-</div>
+package com.taotao.portal.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.taotao.common.utils.HttpClientsUtil;
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.common.utils.TaotaoResult;
+import com.taotao.pojo.TbItemDesc;
+import com.taotao.pojo.TbItemParamItem;
+import com.taotao.portal.pojo.ItemInfo;
+import com.taotao.portal.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Value("${REST_BASE_URL}")
+    private String REST_BASE_URL;
+
+    @Value("${ITEM_INFO_URL}")
+    private String ITEM_INFO_URL;
+
+    @Value("${ITEM_DESC_URL}")
+    private String ITEM_DESC_URL;
+
+    @Value("${ITEM_PARAM_URL}")
+    private String ITEM_PARAM_URL;
+
+    public ItemInfo getItemById(long id) {
+        try {
+            String getJson = HttpClientsUtil.doGet(REST_BASE_URL + ITEM_INFO_URL + id);
+            if (StringUtils.isNotBlank(getJson)) {
+                // 将json转换成对象
+                TaotaoResult taotaoResult = TaotaoResult.formatToPojo(getJson, ItemInfo.class);
+                if (taotaoResult.getStatus() == 200) {
+                    ItemInfo itemInfo = (ItemInfo) taotaoResult.getData();
+                    return itemInfo;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public String getDescById(long id) {
+        try {
+            String getJson = HttpClientsUtil.doGet(REST_BASE_URL + ITEM_DESC_URL + id);
+            if (StringUtils.isNotBlank(getJson)) {
+                TaotaoResult taotaoResult = TaotaoResult.formatToPojo(getJson, TbItemDesc.class);
+                if (taotaoResult.getStatus() == 200) {
+                    TbItemDesc tbItemDesc = (TbItemDesc) taotaoResult.getData();
+                    return tbItemDesc.getItemDesc();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getItemParam(long itemId) {
+        try {
+            String getJson = HttpClientsUtil.doGet(REST_BASE_URL + ITEM_PARAM_URL + itemId);
+            // 把json转换成java对象
+            TaotaoResult result = TaotaoResult.formatToPojo(getJson, TbItemParamItem.class);
+            if (result.getStatus() == 200) {
+                TbItemParamItem itemParamItem = (TbItemParamItem) result.getData();
+                String paramData = itemParamItem.getParamData();
+                // 生成html
+                // 把规格参数json数据转换成java对象
+                List<Map> jsonList = JsonUtils.jsonToList(paramData, Map.class);
+                StringBuffer sb = new StringBuffer();
+                sb.append("<table cellpadding=\"0\" cellspacing=\"1\" width=\"100%\" border=\"0\" class=\"Ptable\">\n");
+                sb.append("    <tbody>\n");
+                for (Map m1 : jsonList) {
+                    sb.append("        <tr>\n");
+                    sb.append("            <th class=\"tdTitle\" colspan=\"2\">" + m1.get("group")
+                            + "</th>\n");
+                    sb.append("        </tr>\n");
+                    List<Map> list2 = (List<Map>) m1.get("params");
+                    for (Map m2 : list2) {
+                        sb.append("        <tr>\n");
+                        sb.append("            <td class=\"tdTitle\">" + m2.get("k") + "</td>\n");
+                        sb.append("            <td>" + m2.get("v") + "</td>\n");
+                        sb.append("        </tr>\n");
+                    }
+                }
+                sb.append("    </tbody>\n");
+                sb.append("</table>");
+                // 返回html片段
+                return sb.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
